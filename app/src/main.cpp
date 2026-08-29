b6012ee7a9e37a37d07a1c81751f706b2f946d93
Edison Carrick <edison.carrick@cognosos.com>
2026-08-28 20:07:10 -0400

    incorporate rgb led on devkit

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,13 +1,16 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/drivers/led_strip.h>
 
 #define SLEEP_TIME_MS 1000
 
 /* The devicetree node identifier for the "led0" alias. */
-#define LED_NODE DT_ALIAS(led0)
+#define STRIP_NODE DT_ALIAS(led_strip)
+const struct device * strip = DEVICE_DT_GET(STRIP_NODE);
 
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
+#define STRIP_NUM_PIXELS 1
+struct led_rgb pixels[STRIP_NUM_PIXELS];
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
@@ -15,14 +18,26 @@ int main(void)
 {
     bool led_state = true;
 
-    if (!gpio_is_ready_dt(&led)) return 0;
+    if (!device_is_ready(strip)) return 0;
 
-    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
+ // Set the first pixel to solid Red
+    pixels[0].r = 255;
+    pixels[0].g = 0;
+    pixels[0].b = 0;
+
+    // Flush color array updates to the NeoPixels
+    led_strip_update_rgb(strip, pixels, STRIP_NUM_PIXELS);
+
 
     while (1) {
-        if (gpio_pin_toggle_dt(&led) < 0) return 0;
+        led_strip_update_rgb(strip, pixels, STRIP_NUM_PIXELS);
 
         led_state = !led_state;
+
+        pixels[0].r = led_state?127:0;
+        pixels[0].g = 0;
+        pixels[0].b = 0;
+
         LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
         k_msleep(SLEEP_TIME_MS);
     }
