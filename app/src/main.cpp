35486128cb6b5ab5204578a9e6c710116ffa3eba
Edison Carrick <edison.carrick@cognosos.com>
2026-09-04 16:43:35 -0400

    add kconfig for LED features

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -3,7 +3,22 @@
 #include <zephyr/logging/log.h>
 #include <zephyr/drivers/led_strip.h>
 
+#ifdef CONFIG_LED_SUBSYSTEM
+#ifdef CONFIG_BLINK_SLEEP_250MS
+#define SLEEP_TIME_MS 250
+#endif
+#ifdef CONFIG_BLINK_SLEEP_1000MS
 #define SLEEP_TIME_MS 1000
+#endif
+#ifdef CONFIG_BLINK_SLEEP_2000MS
+#define SLEEP_TIME_MS 2000
+#endif
+#endif
+
+
+#ifndef SLEEP_TIME_MS
+#error "No sleep time for app"
+#endif
 
 /* The devicetree node identifier for the "led0" alias. */
 #define STRIP_NODE DT_ALIAS(led_strip)
@@ -14,6 +29,15 @@ struct led_rgb pixels[STRIP_NUM_PIXELS];
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
+static uint8_t get_led_level(void) {
+#ifdef CONFIG_LED_ADVANCED
+    uint16_t tmp = (uint16_t)255*(uint16_t)CONFIG_LED_BRIGHTNESS;
+    return (uint8_t)(tmp/100);
+#else
+    return 255;
+#endif
+}
+
 int main(void)
 {
     bool led_state = true;
@@ -21,7 +45,7 @@ int main(void)
     if (!device_is_ready(strip)) return 0;
 
  // Set the first pixel to solid Red
-    pixels[0].r = 255;
+    pixels[0].r = get_led_level();
     pixels[0].g = 0;
     pixels[0].b = 0;
 
@@ -30,14 +54,48 @@ int main(void)
 
 
     while (1) {
-        led_strip_update_rgb(strip, pixels, STRIP_NUM_PIXELS);
 
         led_state = !led_state;
 
-        pixels[0].r = led_state?127:0;
+#ifdef CONFIG_LED_ADVANCED
+        if (led_state) {
+            pixels[0].r = led_state?get_led_level():0;
+            pixels[0].g = 0;
+            pixels[0].b = 0;
+        } else {
+            uint16_t fade_time = CONFIG_LED_FADE_DURATION_MS;
+            uint8_t level = get_led_level();
+            uint8_t fade_level_step = (level*100) / (fade_time);
+            LOG_INF("time: %u level %u step %u", fade_time, level, fade_level_step);
+            while (fade_time) {
+                level -= fade_level_step;
+                LOG_INF("new level %u", level);
+                pixels[0].r = level;
+                pixels[0].g = 0;
+                pixels[0].b = 0;
+
+                led_strip_update_rgb(strip, pixels, STRIP_NUM_PIXELS);
+
+                k_msleep(100);
+                if (fade_time > 100) {
+                    fade_time -= 100;
+                } else {
+                    fade_time = 0;
+                }
+            }
+
+
+        }
+
+#else
+
+        pixels[0].r = led_state?get_led_level():0;
         pixels[0].g = 0;
         pixels[0].b = 0;
 
+        led_strip_update_rgb(strip, pixels, STRIP_NUM_PIXELS);
+
+#endif
         LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
         k_msleep(SLEEP_TIME_MS);
     }
